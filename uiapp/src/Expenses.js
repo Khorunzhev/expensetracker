e9c3e920c0743d77fc8935192a293e5bb1b56b01
Александр Хорунжев <horunzhev@tecforce.ru>
2020-11-04 17:47:13 +0400

    react app in process

--- uiapp/src/Expenses.js
+++ uiapp/src/Expenses.js
@@ -3,40 +3,73 @@ import Navbar from "./AppNav"
 import DatePicker from "react-datepicker"
 import './App.css'
 import "react-datepicker/dist/react-datepicker.css"
-import {Container, FormGroup, Form, Button} from "reactstrap";
+import {Link} from "react-router-dom";
+import {Container, FormGroup, Form, Button, Input, Label} from "reactstrap";
 import AppNav from "./AppNav";
 
 class Expenses extends Component {
-    state = {}
+    state = {
+        date: new Date(),
+        isLoading: true,
+        expenses: [],
+        Categories: []
+    }
     handleChange
+    handleSubmit
+
+    async componentDidMount() {
+        const response = await fetch("/api/categories")
+        const body = await response.json();
+        this.setState({Categories: body, isLoading: false})
+    }
+
     render() {
+        const title = <h3>Add Expense</h3>
+        const {Categories, isLoading} = this.state
+
+        if (isLoading)
+            return (<div>Loading...</div>)
+
+        let optionList = Categories.map(category =>
+            <option id = {category.id}>
+                {category.name}
+            </option>
+        )
         return (
             <div>
                 <AppNav/>
                     <Container>
-                        <Form>
+                        {title}
+                        <Form onSubmit={this.handleSubmit}>
                             <FormGroup>
-                                <label for="title">Title</label>
-                                <input type="text" name='title' id="title" onChange={this.handleChange}></input>
+                                <Label for="title">Title</Label>
+                                <Input type="text" name='title' id="title"
+                                       onChange={this.handleChange}></Input>
                             </FormGroup>
 
                             <FormGroup>
-                                <label for="category">Category</label>
-                                <input type="text" name='category' id="category" onChange={this.handleChange}></input>
+                                <Label for="category">Category</Label>
+                                <select>
+                                    {optionList}
+                                </select>
+                                <Input type="text" name='category' id="category"
+                                       onChange={this.handleChange}></Input>
                             </FormGroup>
 
                             <FormGroup>
-                                <label for="expenseDate">Expense Date</label>
-                                <input type="text" name='expenseDate' id="expenseDate" onChange={this.handleChange}></input>
+                                <Label for="expenseDate">Expense Date</Label>
+                                <DatePicker selected={this.state.date} onChange={this.handleChange}/>
                             </FormGroup>
 
                             <FormGroup>
-                                <label for="location">Location</label>
-                                <input type="text" name='location' id="location" onChange={this.handleChange}></input>
+                                <Label for="location">Location</Label>
+                                <Input type="text" name='location' id="location"
+                                       onChange={this.handleChange}></Input>
                             </FormGroup>
 
                             <FormGroup>
                                 <Button color="primary" type="submit">Save</Button>{' '}
+                                <Button color="secondary" tag={Link} to="/categories">Cancel</Button>
                             </FormGroup>
                         </Form>
                     </Container>
