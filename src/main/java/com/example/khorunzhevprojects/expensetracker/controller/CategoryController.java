a6cfccead864c16ed340da7e7b8a12df92e78639
Александр Хорунжев <horunzhev@tecforce.ru>
2020-11-04 13:48:56 +0400

    spring boot

---
+++ src/main/java/com/example/khorunzhevprojects/expensetracker/controller/CategoryController.java
@@ -0,0 +1,53 @@
+package com.example.khorunzhevprojects.expensetracker.controller;
+
+import com.example.khorunzhevprojects.expensetracker.model.Category;
+import com.example.khorunzhevprojects.expensetracker.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping("/categories")
+    Collection<Category> categories() {
+        return  categoryRepository.findAll();
+    }
+
+    @GetMapping("category/{id}")
+    ResponseEntity<Category> getCategory(@PathVariable Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return category.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/category")
+    ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.created(new URI(String.format("api/category/%d", result.getId()))).body(result);
+    }
+
+    @PutMapping("/category/{id}")
+    ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @DeleteMapping("/category/{id}")
+    ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        categoryRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
--- src/main/java/com/example/khorunzhevprojects/expensetracker/repository/CategoryRepository.java
+++ src/main/java/com/example/khorunzhevprojects/expensetracker/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.example.khorunzhevprojects.expensetracker.repository;
 
 import com.example.khorunzhevprojects.expensetracker.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Category findByName(String name);
